7cc968842ea035b7eb25c28919f889e3e2f956ea
Ashborn <75546030+AshbornXS@users.noreply.github.com>
2023-04-15 19:00:29 -0300

    mudança de db & 'esqueceu a senha'(parcialmente funcional)

--- app/src/main/java/com/rae/daply/login/LoginActivity.kt
+++ app/src/main/java/com/rae/daply/login/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.rae.daply.login
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -51,10 +55,44 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        binding.forgotPassword.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.dialog_forgot, null)
+            val userEmail = view.findViewById<EditText>(R.id.editBox)
+            builder.setView(view)
+            val dialog = builder.create()
+            view.findViewById<Button>(R.id.btnReset).setOnClickListener {
+                compareEmail(userEmail)
+                dialog.dismiss()
+            }
+            view.findViewById<Button>(R.id.btnCancel).setOnClickListener {
+                dialog.dismiss()
+            }
+            if (dialog.window != null) {
+                dialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+            }
+            dialog.show()
+        }
+
         binding.signupRedirectText.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun compareEmail(email: EditText){
+        if (email.text.toString().isEmpty()){
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()){
+            return
+        }
+        firebaseAuth.sendPasswordResetEmail(email.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Check your email", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onStart() {
